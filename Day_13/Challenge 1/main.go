096a5f08d4af4a7526b7c1f4e16501bdb126ba32
Yorick Smilda <y.smilda@gmail.com>
2018-12-14 22:05:04 +0100

    Day 13 commit

--- Day_13/Challenge 1/main.go
+++ Day_13/Challenge 1/main.go
@@ -9,19 +9,6 @@ import (
 	"time"
 )
 
-func checkDirection(trackMap []string) (int, int) {
-	x := 0
-	y := 0
-
-	for i := 0; i < len(trackMap); i++ {
-		for j := 0; j < len(trackMap[i]); j++ {
-
-		}
-	}
-
-	return x, y
-}
-
 func updateCartsPositions(cartPositions [][]int, trackMap []string) [][]int {
 	for i := 0; i < len(cartPositions); i++ {
 		x := cartPositions[i][0]
@@ -32,44 +19,116 @@ func updateCartsPositions(cartPositions [][]int, trackMap []string) [][]int {
 
 		case 0:
 			switch string(trackMap[x-1][y]) {
-			case "\\":
+			case "\\": //left
 				cartPositions[i][2] = 3
-			case "/":
+			case "/": //right
 				cartPositions[i][2] = 1
 			case "+":
 				switch cartPositions[i][3] {
-				case 0:
-					cartPositions
-				case 1:
-				case 2:
+				case 0: //left
+					cartPositions[i][3] = 1
+					cartPositions[i][2] = 3
+				case 1: //straight
+					cartPositions[i][3] = 2
+				case 2: //right
+					cartPositions[i][3] = 0
+					cartPositions[i][2] = 1
+				}
+			}
+			cartPositions[i][0] = x - 1
+		case 1:
+			switch string(trackMap[x][y+1]) {
+			case "\\": //down
+				cartPositions[i][2] = 2
+			case "/": //up
+				cartPositions[i][2] = 0
+			case "+":
+				switch cartPositions[i][3] {
+				case 0: //up
+					cartPositions[i][3] = 1
+					cartPositions[i][2] = 0
+				case 1: //straight
+					cartPositions[i][3] = 2
+				case 2: //down
+					cartPositions[i][3] = 0
+					cartPositions[i][2] = 2
+				}
+			}
+			cartPositions[i][1] = y + 1
+		case 2:
+			switch string(trackMap[x+1][y]) {
+			case "\\": //right
+				cartPositions[i][2] = 1
+			case "/": //left
+				cartPositions[i][2] = 3
+			case "+":
+				switch cartPositions[i][3] {
+				case 0: //right
+					cartPositions[i][3] = 1
+					cartPositions[i][2] = 1
+				case 1: //straight
+					cartPositions[i][3] = 2
+				case 2: //left
+					cartPositions[i][3] = 0
+					cartPositions[i][2] = 3
 				}
 			}
 			cartPositions[i][0] = x + 1
-		case 1:
-			switch string(trackMap[x][y+1]) {
-			case "-":
-			case "\\":
-			case "/":
-			case "+":
-			}
-		case 2:
-			switch string(trackMap[x+1][y]) {
-			case "|":
-			case "\\":
-			case "/":
-			case "+":
-			}
 		case 3:
-			switch string(trackMap[x][y] - 1) {
-			case "-":
-			case "\\":
-			case "/":
+			switch string(trackMap[x][y-1]) {
+			case "\\": //up
+				cartPositions[i][2] = 0
+			case "/": //down
+				cartPositions[i][2] = 2
 			case "+":
+				switch cartPositions[i][3] {
+				case 0: //down
+					cartPositions[i][3] = 1
+					cartPositions[i][2] = 2
+				case 1: //straight
+					cartPositions[i][3] = 2
+				case 2: //up
+					cartPositions[i][3] = 0
+					cartPositions[i][2] = 0
+				}
 			}
+			cartPositions[i][1] = y - 1
 		}
 	}
 
 	return cartPositions
+}
+
+func drawTracks(trackMap []string, carts [][]int) {
+	for i := 0; i < len(trackMap[0]); i++ {
+		fmt.Print(i / 100)
+	}
+	fmt.Println("")
+	for i := 0; i < len(trackMap[0]); i++ {
+		fmt.Print(i % 100 / 10)
+	}
+	fmt.Println("")
+	for i := 0; i < len(trackMap[0]); i++ {
+		fmt.Print(i % 10)
+	}
+	fmt.Println("")
+
+	for i := 0; i < len(trackMap); i++ {
+		for j := 0; j < len(trackMap[i]); j++ {
+			print := true
+			for k := 0; k < len(carts); k++ {
+				if carts[k][0] == i && carts[k][1] == j {
+					fmt.Print("#")
+					print = false
+				}
+			}
+			if print {
+				fmt.Print(string(trackMap[i][j]))
+			}
+
+		}
+		fmt.Println("")
+	}
 }
 
 func getCartPositions(trackMap []string) [][]int {
@@ -93,12 +152,12 @@ func getCartPositions(trackMap []string) [][]int {
 				direction = 1
 				cartPositions = append(cartPositions, []int{x, y, direction, 0})
 				trackMap[i] = strings.Replace(trackMap[i], ">", "-", 1)
-			case "V":
+			case "v":
 				x = i
 				y = j
 				direction = 2
 				cartPositions = append(cartPositions, []int{x, y, direction, 0})
-				trackMap[i] = strings.Replace(trackMap[i], "V", "|", 1)
+				trackMap[i] = strings.Replace(trackMap[i], "v", "|", 1)
 			case "<":
 				x = i
 				y = j
@@ -113,15 +172,16 @@ func getCartPositions(trackMap []string) [][]int {
 	return cartPositions
 }
 
-func colisionDetect(cartPositions [][]int) bool {
+func colisionDetect(cartPositions [][]int) (bool, []int) {
 	for i := 0; i < len(cartPositions); i++ {
-		for j := 0; j < len(cartPositions); j++ {
+		for j := i + 1; j < len(cartPositions); j++ {
 			if cartPositions[i][0] == cartPositions[j][0] && cartPositions[i][1] == cartPositions[j][1] {
-				return true
+				coordinates := []int{cartPositions[i][0], cartPositions[i][1]}
+				return true, coordinates
 			}
 		}
 	}
-	return false
+	return false, nil
 }
 
 func main() {
@@ -129,8 +189,9 @@ func main() {
 
 	trackMap := []string{}
 	carts := [][]int{}
+	found := false
 
-	crash := false
+	var coordinates []int
 
 	intputFile, err := os.Open("input.txt")
 
@@ -146,17 +207,15 @@ func main() {
 
 	carts = getCartPositions(trackMap)
 
-	for !crash {
-		carts = updateCartsPositions(carts, trackMap)
-
-		crash = colisionDetect(carts)
-	}
-
+	fmt.Println(len(trackMap), len(trackMap[1]))
 	fmt.Println(carts)
 
-	// for i := 0; i < len(trackMap); i++ {
-	// 	fmt.Println(trackMap[i])
-	// }
+	for !found {
+		carts = updateCartsPositions(carts, trackMap)
+		found, coordinates = colisionDetect(carts)
+	}
+
+	fmt.Println(coordinates[1], coordinates[0])
 
 	fmt.Println(time.Since(start))
 }
