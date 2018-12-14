5494cb99e51a2c443ba422d6b4f4a52464eb51f6
Yorick Smilda <y.smilda@gmail.com>
2018-12-14 10:27:07 +0100

    implemented SAT, huge speed improvement

--- Day 11/Challenge 2/main.go
+++ Day 11/Challenge 2/main.go
@@ -23,24 +23,35 @@ func drawGrid(gridSize int, gridID int) (grid [][]int) {
 	return grid
 }
 
-func countPowerLevel(input [][]int) (coordinates []int) {
+func createSAT(grid [][]int) [][]int {
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if i == 0 && j > 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if i > 0 && j == 0 {
+				grid[i][j] += grid[i-1][j]
+			} else if i > 0 && j > 0 {
+				grid[i][j] += grid[i-1][j] + grid[i][j-1] - grid[i-1][j-1]
+			}
+		}
+	}
+
+	return grid
+}
+
+func countPowerLevel(input [][]int, gridSize int) (coordinates []int) {
 	max := 0
 	count := 0
 
-	for a := 0; a < 300; a++ {
+	for a := 1; a < gridSize; a++ {
 		fmt.Println(a)
-		for i := 0; i < len(input)-a-1; i++ {
-			for j := 0; j < len(input[i])-a-1; j++ {
-				count = 0
-				for k := 0; k < a; k++ {
-					for l := 0; l < a; l++ {
-						count += input[i+k][j+l]
-					}
-				}
+		for i := len(input) - 1; i > 0+a; i-- {
+			for j := len(input[i]) - 1; j > 0+a; j-- {
+				count = input[i][j] - input[i-a][j] - input[i][j-a] + input[i-a][j-a]
 				if count > max {
-					fmt.Println(count)
 					max = count
-					coordinates = []int{i, j, a}
+					coordinates = []int{i - a + 1, j - a + 1, a}
 				}
 			}
 		}
@@ -57,8 +68,12 @@ func main() {
 	coordinates := []int{}
 
 	grid := drawGrid(gridSize, gridID)
+	// fmt.Println(grid[2])
 
-	coordinates = countPowerLevel(grid)
+	grid = createSAT(grid)
+	// fmt.Println(grid[2])
+
+	coordinates = countPowerLevel(grid, gridSize)
 
 	fmt.Println(coordinates)
 
