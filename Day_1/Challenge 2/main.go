d22ecbc5eaf9432fc6f89a2cf79da3d64c41f77e
Yorick Smilda <y.smilda@gmail.com>
2018-12-14 21:01:21 +0100

    Refactor to improve speed

--- Day_1/Challenge 2/main.go
+++ Day_1/Challenge 2/main.go
@@ -9,42 +9,61 @@ import (
 	"time"
 )
 
+func checkValue(frequency int, outputRecord []int) bool {
+	for i := 0; i < len(outputRecord); i++ {
+		if outputRecord[i] == frequency {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	start := time.Now()
-	frequency := 0
-	var outputRec []int
 
-	for {
-		intputFile, err := os.Open("input.txt")
+	frequency := 0
+	input := []int{}
+	outputRecord := []int{}
+
+	found := false
+
+	intputFile, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer intputFile.Close()
+
+	scanner := bufio.NewScanner(intputFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		v, err := strconv.Atoi(line)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		defer intputFile.Close()
+		input = append(input, v)
+	}
 
-		scanner := bufio.NewScanner(intputFile)
-
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			v, err := strconv.Atoi(line)
-
-			if err != nil {
-				log.Fatal(err)
+	for !found {
+		for i := 0; i < len(input); i++ {
+			frequency += input[i]
+			found = checkValue(frequency, outputRecord)
+			if found == true {
+				break
+			} else {
+				outputRecord = append(outputRecord, frequency)
 			}
-
-			frequency += v
-
-			for i := 0; i < len(outputRec); i++ {
-				if frequency == outputRec[i] {
-					fmt.Println(frequency)
-					fmt.Println(time.Since(start))
-					return
-				}
-			}
-
-			outputRec = append(outputRec, frequency)
 		}
 	}
+
+	fmt.Println(frequency)
+
+	fmt.Println(time.Since(start))
+
 }
