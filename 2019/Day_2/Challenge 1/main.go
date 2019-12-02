7f679f0193c411b352faf25a55ae4f93a233fee9
Yorick Smilda <y.smilda@gmail.com>
2019-12-02 09:20:51 +0100

    working

--- 2019/Day_2/Challenge 1/main.go
+++ 2019/Day_2/Challenge 1/main.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-
-	opcode := [100]strig{}
 
 	inputFile, err := os.Open("input.txt")
 
@@ -26,7 +25,24 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		opcode = strings.Split(line, ",")
+		opcodeString := strings.Split(line, ",")
+		opcode := []int{}
+		for _, codes := range opcodeString {
+			i, _ := strconv.Atoi(codes)
+			opcode = append(opcode, i)
+		}
+
+		for i := 0; i < len(opcode); i += 4 {
+
+			if opcode[i] == 1 {
+				opcode[opcode[i+3]] = opcode[opcode[i+1]] + opcode[opcode[i+2]]
+			} else if opcode[i] == 2 {
+				opcode[opcode[i+3]] = opcode[opcode[i+1]] * opcode[opcode[i+2]]
+			} else if opcode[i] == 99 {
+				break
+			}
+		}
+		fmt.Println(opcode[0])
 	}
 
 	fmt.Println(time.Since(start))
