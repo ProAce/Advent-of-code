53e44c25437e874296a9e77a95f04221bc8c7b7a
Yorick Smilda <y.smilda@acs-buildings.com>
2019-12-04 11:00:57 +0100

    WIP

---
+++ 2019/Day_4/Challenge 2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	counter := 0
+	for i := 372304; i <= 847060; i++ { // Puzzle input
+		if criteriaCheck(i) {
+			counter++
+		}
+	}
+	fmt.Println(counter)
+
+	fmt.Println(time.Since(start))
+}
+
+func criteriaCheck(i int) bool {
+	digits := []int{}
+	for i > 0 {
+		digits = append(digits, (i % 10))
+		i /= 10
+	}
+
+	double := false
+
+	for j := 0; j < len(digits)-1; j++ {
+		if digits[j] < digits[j+1] {
+			return false
+		}
+
+		if digits[j] == digits[j+1] {
+			double = true
+		}
+	}
+
+	return double
+}
