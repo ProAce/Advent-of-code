adebd97570fa296aff55f900d85ed4d04bab1582
Yorick Smilda <y.smilda@acs-buildings.com>
2019-12-04 09:19:10 +0100

    add time

--- 2019/Day_4/Challenge 1/main.go
+++ 2019/Day_4/Challenge 1/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main(){
+func main() {
+	start := time.Now()
+
 	counter := 0
-	for i := 372304; i <= 847060; i++{ // Puzzle input
+	for i := 372304; i <= 847060; i++ { // Puzzle input
 		if criteriaCheck(i) {
 			counter++
 		}
 	}
 	fmt.Println(counter)
+
+	fmt.Println(time.Since(start))
 }
 
-func criteriaCheck(i int) bool{
+func criteriaCheck(i int) bool {
 	digits := []int{}
 	for i > 0 {
 		digits = append(digits, (i % 10))
@@ -22,15 +27,16 @@ func criteriaCheck(i int) bool{
 	}
 
 	same := false
-	
-	for j := 0; j < len(digits) - 1; j++{
-		if digits[j] < digits[j + 1]{
+
+	for j := 0; j < len(digits)-1; j++ {
+		if digits[j] < digits[j+1] {
 			return false
 		}
-		if digits[j] == digits[j + 1]{ 
-			same = true;
+
+		if digits[j] == digits[j+1] {
+			same = true
 		}
 	}
 
-	return same;
+	return same
 }
